6d8da6ee3ff5e9fcf71ff0d2f912dfbfd26b6f13
Parth <parth280197@gmail.com>
2019-11-14 10:18:18 -0600

    Seeding method updated to add questions

--- AskMe/Helper/SeedingHelper.cs
+++ AskMe/Helper/SeedingHelper.cs
@@ -4,39 +4,77 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace AskMe.Helper
 {
-    public class SeedingHelper
+  public class SeedingHelper
+  {
+    UserManager<User> userManager;
+    RoleManager<IdentityRole> roleManager;
+    ApplicationDbContext context;
+    public SeedingHelper(ApplicationDbContext context)
     {
-        UserManager<User> userManager;
-        RoleManager<IdentityRole> roleManager;
-        ApplicationDbContext context;
-        public SeedingHelper(ApplicationDbContext context)
-        {
-            this.context = context;
-            userManager = new UserManager<User>(new UserStore<User>(context));
-            roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-        }
-
-        public void CreateUser(string name)
-        {
-            string userName = name + "@AskMe.com";
-            string password = name + "A123";
-
-            var userInDb = userManager.FindByName(userName);
-            if (userInDb != null) return;
-            var PasswordHash = new PasswordHasher();
-            var user = new User()
-            {
-                UserName = userName,
-                Email = userName,
-                PasswordHash = PasswordHash.HashPassword(password),
-            };
-            userManager.Create(user);
-        }
-    
-
+      this.context = context;
+      userManager = new UserManager<User>(new UserStore<User>(context));
+      roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
     }
+
+    public void CreateUser(string name)
+    {
+      string userName = name + "@AskMe.com";
+      string password = name + "A123";
+
+      var userInDb = userManager.FindByName(userName);
+      if (userInDb != null) return;
+      var PasswordHash = new PasswordHasher();
+      var user = new User()
+      {
+        UserName = userName,
+        Email = userName,
+        PasswordHash = PasswordHash.HashPassword(password),
+      };
+      userManager.Create(user);
+    }
+    public void CreateTags()
+    {
+      List<Tag> tags = new List<Tag>()
+      {
+        new Tag() {Name="C#"},
+        new Tag() {Name="Asp.net"},
+        new Tag() {Name="Sql"},
+        new Tag() {Name="Css"},
+        new Tag() {Name="Html"},
+        new Tag() {Name="Js"},
+      };
+      foreach (var tag in tags)
+      {
+        var isExist = context.Tags.Any(t => t.Name == tag.Name);
+        if (!isExist)
+        {
+          context.Tags.Add(tag);
+        }
+      }
+    }
+
+    public void AddQuestions()
+    {
+      Post post = new Post()
+      {
+        Content = "<h1>I know you can answer this question?</h1>",
+        Title = "Question",
+        CreatedTime = DateTime.Now,
+        CreatedBy = context.Users.Where(user => user.UserName == "User1@AskMe.com").FirstOrDefault()
+      };
+
+      Question question = new Question()
+      {
+        Post = post,
+        Tags = context.Tags.Take(2).ToList(),
+      };
+      context.Posts.Add(post);
+      context.SaveChanges();
+      context.Questions.Add(question);
+      context.SaveChanges();
+    }
+  }
 }
--- AskMe/Migrations/Configuration.cs
+++ AskMe/Migrations/Configuration.cs
@@ -1,42 +1,45 @@
 namespace AskMe.Migrations
 {
-    using AskMe.Helper;
-    using AskMe.Models;
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+  using AskMe.Helper;
+  using AskMe.Models;
+  using Microsoft.AspNet.Identity;
+  using Microsoft.AspNet.Identity.EntityFramework;
+  using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<AskMe.Models.ApplicationDbContext>
+  internal sealed class Configuration : DbMigrationsConfiguration<AskMe.Models.ApplicationDbContext>
+  {
+    public Configuration()
     {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
-
-        protected override void Seed(AskMe.Models.ApplicationDbContext context)
-        {
-
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
-
-            SeedingHelper seedingHelper = new SeedingHelper(context);
-            var userManager = new UserManager<User>(new UserStore<User>(context));
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            //Add role
-            //var roles = new string[] { "Admin", "User" };
-            //seedingHelper.CreatRoles(roles);
-
-            //Add user
-            seedingHelper.CreateUser("User1");
-            seedingHelper.CreateUser("User2");
-            seedingHelper.CreateUser("User3");
-
-
-        }
+      AutomaticMigrationsEnabled = false;
     }
+
+    protected override void Seed(AskMe.Models.ApplicationDbContext context)
+    {
+
+      //  This method will be called after migrating to the latest version.
+
+      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+      //  to avoid creating duplicate seed data.
+
+      SeedingHelper seedingHelper = new SeedingHelper(context);
+      var userManager = new UserManager<User>(new UserStore<User>(context));
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+      //Add role
+      //var roles = new string[] { "Admin", "User" };
+      //seedingHelper.CreatRoles(roles);
+
+      //Add user
+      seedingHelper.CreateUser("User1");
+      seedingHelper.CreateUser("User2");
+      seedingHelper.CreateUser("User3");
+
+      //Add Tags
+      seedingHelper.CreateTags();
+
+      //Add Question
+      seedingHelper.AddQuestions();
+
+
+    }
+  }
 }

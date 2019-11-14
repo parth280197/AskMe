95d16bcdb6a0cef2f38a3bf9f6056151a2253fbc
Parth <parth280197@gmail.com>
2019-11-14 09:36:04 -0600

    Select2 installed

--- AskMe/Controllers/QuestionController.cs
+++ AskMe/Controllers/QuestionController.cs
@@ -1,34 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using AskMe.BusinessLayer;
+﻿using AskMe.BusinessLayer;
 using AskMe.Models;
+using AskMe.Models.ViewModel;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace AskMe.Controllers
 {
-    public class QuestionController : Controller
+  public class QuestionController : Controller
+  {
+    QuestionBusinessLayer QBL = new QuestionBusinessLayer();
+
+    // GET: Question
+    public ActionResult Index()
     {
-        QuestionBusinessLayer QBL = new QuestionBusinessLayer();
-
-        // GET: Question
-        public ActionResult Index()
-        {
-            var allQuestions = QBL.GetAllQuestions();
-            return View();
-        }
-
-        public ActionResult CreateQuestion()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult CreateQuestion(Question question)
-        {
-
-            return View();
-        }
+      var allQuestions = QBL.GetAllQuestions();
+      return View();
     }
+
+    public ActionResult CreateQuestion()
+    {
+      QuestionCreateViewModel viewModel = new QuestionCreateViewModel()
+      {
+        Tags = new List<Tag>()
+        {
+          new Tag() {Id=1,Name="Tag1"}
+        }
+      };
+      return View(viewModel);
+    }
+    [HttpPost]
+    [ValidateInput(false)]
+    public ActionResult CreateQuestion(QuestionCreateViewModel question)
+    {
+
+      return View();
+    }
+  }
 }
---
+++ AskMe/Models/ViewModel/QuestionCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AskMe.Models.ViewModel
+{
+  public class QuestionCreateViewModel
+  {
+    public string Content { get; set; }
+    public string Title { get; set; }
+    public List<Tag> Tags { get; set; }
+    public int[] TagsId { get; set; }
+    public string NewTags { get; set; }
+  }
+}

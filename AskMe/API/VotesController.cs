d97e0bfd73a209bef54aa01b5effb764476d7dde
abhi <abrahmbhatt03@gmai.com>
2019-11-19 20:52:58 -0600

    Some changes in controller

---
+++ AskMe/API/VotesController.cs
@@ -0,0 +1,36 @@
+﻿using AskMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AskMe.API
+{
+    public class VotesController : ApiController
+    {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        public IHttpActionResult UpVote(int postId)
+        {
+            var user = User.Identity.GetUserId();
+            var post = _context.Posts.Find(postId);
+            var votes = post.Votes.ToList();
+            if (votes.FindAll(u => u.UserId == userId).Count == 0)
+            {
+                Votes vote = new Votes
+                {
+                    VotedDateTime = DateTime.Now,
+                    UserId = userId,
+                    User = user,
+                    Post = post,
+                    PostId = postId,
+                };
+                post.Votes.Add(vote);
+                _context.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
--- AskMe/Models/ViewModel/QuestionDetailViewModel.cs
+++ AskMe/Models/ViewModel/QuestionDetailViewModel.cs
@@ -2,12 +2,14 @@
 
 namespace AskMe.Models.ViewModel
 {
-  public class QuestionDetailViewModel
-  {
-    public Question Question { get; set; }
-    [Display(Name = "Add your answer.")]
-    public string AnswerContent { get; set; }
-    public string UserId { get; set; }
-    public int QuestionId { get; set; }
-  }
+    public class QuestionDetailViewModel
+    {
+        public Question Question { get; set; }
+        [Display(Name = "Add your answer.")]
+        public string AnswerContent { get; set; }
+        public string UserId { get; set; }
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; }
+        public int ViewCount { get; set; }
+    }
 }

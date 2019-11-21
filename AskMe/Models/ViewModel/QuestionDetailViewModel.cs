6eb6597a2a9c9956c842bd498f7460cb7ff0e399
abhi <abrahmbhatt03@gmai.com>
2019-11-21 14:51:14 -0600

    added count in queton detail model.

--- AskMe/Models/ViewModel/QuestionDetailViewModel.cs
+++ AskMe/Models/ViewModel/QuestionDetailViewModel.cs
@@ -4,12 +4,16 @@ namespace AskMe.Models.ViewModel
 {
     public class QuestionDetailViewModel
     {
+        public QuestionDetailViewModel()
+        {
+            VoteCount = 0;
+        }
         public Question Question { get; set; }
         [Display(Name = "Add your answer.")]
         public string AnswerContent { get; set; }
         public string UserId { get; set; }
         public int QuestionId { get; set; }
         public int AnswerId { get; set; }
-        public int ViewCount { get; set; }
+        public int VoteCount { get; set; }
     }
 }

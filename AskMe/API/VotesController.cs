a14ed9614537fca988cea62affe11be5a6ca03a0
abhi <abrahmbhatt03@gmai.com>
2019-11-21 15:05:27 -0600

    added votes contoller in master

--- AskMe/API/VotesController.cs
+++ AskMe/API/VotesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AskMe.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +11,29 @@ namespace AskMe.API
 {
     public class VotesController : ApiController
     {
-        //public IHttpActionResult Post
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        [HttpPost]
+        public IHttpActionResult UpVote(int id)
+        {
+            var user = _context.Users.Find(User.Identity.GetUserId());
+            var post = _context.Posts.Find(id);
+            var votes = post.Votes.ToList();
+            if (votes.FindAll(u => u.UserId == user.Id).Count == 0)
+            {
+                Votes vote = new Votes
+                {
+                    VotedDateTime = DateTime.Now,
+                    UserId = user.Id,
+                    User = user,
+                    Post = post,
+                    PostId = id,
+                };
+                post.Votes.Add(vote);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
     }
 }

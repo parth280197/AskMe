113d336ccd8f147fdeec1606763ea3c012e3ea60
Parth <parth280197@gmail.com>
2019-11-21 19:51:18 -0600

    Upvote and DownVote Working using ajax

--- AskMe/API/VotesController.cs
+++ AskMe/API/VotesController.cs
@@ -1,39 +1,51 @@
 ﻿using AskMe.Models;
 using Microsoft.AspNet.Identity;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace AskMe.API
 {
-    public class VotesController : ApiController
-    {
-        ApplicationDbContext _context = new ApplicationDbContext();
+  public class VotesController : ApiController
+  {
+    ApplicationDbContext _context = new ApplicationDbContext();
 
-        [HttpPost]
-        public IHttpActionResult UpVote(int id)
+    [HttpPost]
+    public IHttpActionResult UpVote(int id)
+    {
+      var user = _context.Users.Find(User.Identity.GetUserId());
+      var post = _context.Posts.Find(id);
+      var votes = post.Votes.ToList();
+      if (votes.FindAll(u => u.UserId == user.Id).Count == 0)
+      {
+        Votes vote = new Votes
         {
-            var user = _context.Users.Find(User.Identity.GetUserId());
-            var post = _context.Posts.Find(id);
-            var votes = post.Votes.ToList();
-            if (votes.FindAll(u => u.UserId == user.Id).Count == 0)
-            {
-                Votes vote = new Votes
-                {
-                    VotedDateTime = DateTime.Now,
-                    UserId = user.Id,
-                    User = user,
-                    Post = post,
-                    PostId = id,
-                };
-                post.Votes.Add(vote);
-                _context.SaveChanges();
-                return Ok();
-            }
-            return BadRequest();
-        }
+          VotedDateTime = DateTime.Now,
+          UserId = user.Id,
+          User = user,
+          Post = post,
+          PostId = id,
+        };
+        post.Votes.Add(vote);
+        _context.SaveChanges();
+        return Ok();
+      }
+      return BadRequest();
     }
+
+    public IHttpActionResult DownVote(int id)
+    {
+      var user = _context.Users.Find(User.Identity.GetUserId());
+      var post = _context.Posts.Find(id);
+      var votes = post.Votes.ToList();
+      if (votes.FindAll(u => u.UserId == user.Id).Count == 1)
+      {
+        var voteInDb = _context.Votes.Where(v => v.PostId == id && v.UserId == user.Id).FirstOrDefault();
+        _context.Votes.Remove(voteInDb);
+        _context.SaveChanges();
+        return Ok();
+      }
+      return BadRequest();
+    }
+  }
 }
